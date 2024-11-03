af551c7c5b0a9f1bf2ae7ace95216c3cde55f17a
Quidden <dogemmoze@gmail.com>
2024-11-03 16:22:22 +0200

    Change inventory 2

--- Inventory.h
+++ Inventory.h
@@ -6,105 +6,111 @@
 class Inventory
 {
 public:
-    std::vector<ItemC*> items;           
-    std::vector<ItemC*> activeItems;      
-    int baseDamage = 10;                
+    std::vector<ItemC*> items;
+    ItemC* equipped_weapon = nullptr;
  
-    void AddItem(Weapon* item, GameC* character)
+    void AddItem(ItemC* item, GameC* character)
     {
-        if (character->CanEquip(item->weapon_type))
+        if (Weapon* weapon = dynamic_cast<Weapon*>(item))
         {
-            items.push_back(item);
-            std::cout << character->name << " added item: " << item->name << " to inventory." << std::endl;
-        }
-        else
-        {
-            std::cout << character->name << " cannot equip " << item->name << std::endl;
-        }
-    }
-  
-    void AddItem(ItemC* item)
-    {
-        items.push_back(item);
-        std::cout << "Added item: " << item->name << std::endl;
-    }
-
-    void EquipItem(int index, GameC* character)
-    {
-        if (index >= 0 && index < items.size())
-        {
-            Weapon* weapon = dynamic_cast<Weapon*>(items[index]);
-            if (weapon && character->CanEquip(weapon->weapon_type))
+            if (character->CanEquip(weapon->weapon_type))
             {
-                activeItems.push_back(weapon);    
-                items.erase(items.begin() + index); 
-                std::cout << character->name << " equipped: " << weapon->name << std::endl;
+                items.push_back(item);
+                std::cout << character->name << " added item: " << item->name << std::endl;
             }
             else
             {
-                std::cout << character->name << " cannot equip " << items[index]->name << std::endl;
+                std::cout << character->name << " cannot equip " << item->name << std::endl;
             }
         }
         else
         {
-            std::cout << "Invalid index." << std::endl;
-        }
-    }
-   
-    void UnequipItem(int index)
-    {
-        if (index >= 0 && index < activeItems.size())
-        {
-            items.push_back(activeItems[index]);
-            std::cout << "Unequipped item: " << activeItems[index]->name << std::endl;
-            activeItems.erase(activeItems.begin() + index);
-        }
-        else
-        {
-            std::cout << "Invalid index." << std::endl;
-        }
-    }
-   
-    int GetTotalDamage() const
-    {
-        int totalDamage = baseDamage;
-        for (const ItemC* item : activeItems)
-        {
-            const Weapon* weapon = dynamic_cast<const Weapon*>(item);
-            if (weapon)
-            {
-                totalDamage += weapon->damage;  
-            }
-        }
-        return totalDamage;
-    }
- 
-    void DisplayInventory() const
-    {
-        std::cout << "Inventory contents:" << std::endl;
-        for (int i = 0; i < items.size(); ++i)
-        {
-            std::cout << i << ": " << items[i]->name << " - " << items[i]->description << std::endl;
+            items.push_back(item);
+            std::cout << "Added item: " << item->name << std::endl;
         }
     }
 
-    void DisplayEquippedItems() const
+    void EquipWeapon(int index, GameC* character)
     {
-        std::cout << "Equipped items:" << std::endl;
-        for (int i = 0; i < activeItems.size(); ++i)
+        if (index >= 0 && index < items.size())
         {
-            std::cout << i << ": " << activeItems[i]->name << " - " << activeItems[i]->description << std::endl;
+            if (Weapon* weapon = dynamic_cast<Weapon*>(items[index]))
+            {
+                if (character->CanEquip(weapon->weapon_type))
+                {
+                    equipped_weapon = weapon;
+                    std::cout << character->name << " equipped " << weapon->name << std::endl;
+                }
+                else
+                {
+                    std::cout << character->name << " cannot equip this weapon type." << std::endl;
+                }
+            }
+            else
+            {
+                std::cout << "Selected item is not a weapon." << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << "Invalid index." << std::endl;
+        }
+    }
+   
+    void UseItem(int index, GameC* character)
+    {
+        if (index >= 0 && index < items.size())
+        {
+            if (Health_PotkaC* potion = dynamic_cast<Health_PotkaC*>(items[index]))
+            {
+                std::cout << "Using " << potion->name << " to restore " << potion->health << " health points." << std::endl;
+                character->hp += potion->health;
+                delete items[index];
+                items.erase(items.begin() + index); 
+            }
+            else
+            {
+                std::cout << "Selected item is not a potion." << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << "Invalid index." << std::endl;
+        }
+    }
+   
+    void DisplayInventory()
+    {
+        std::cout << "Inventory contents:" << std::endl;
+        for (int i = 0; i < items.size(); ++i) {
+            std::cout << i << ": " << items[i]->name << " - " << items[i]->description << std::endl;
+        }
+    }
+ 
+    int GetTotalDamage(int base_damage)
+    {
+        base_damage += (equipped_weapon ? dynamic_cast<Weapon*>(equipped_weapon)->damage : 0);
+        return base_damage;
+    }
+
+    void SellItem(int index, int& player_gold)
+    {
+        if (index >= 0 && index < items.size())
+        {
+            player_gold += items[index]->price;
+            std::cout << "Sold item: " << items[index]->name << " for " << items[index]->price << " gold." << std::endl;
+            delete items[index];
+            items.erase(items.begin() + index); // Удаляем предмет из инвентаря
+        }
+        else
+        {
+            std::cout << "Invalid index." << std::endl;
         }
     }
   
     ~Inventory()
     {
-        for (ItemC* item : items)
-        {
-            delete item;
-        }
-        for (ItemC* item : activeItems)
-        {
+        for (ItemC* item : items) {
             delete item;
         }
     }
