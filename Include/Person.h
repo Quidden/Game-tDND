e600e49b10749c04246a13dd2c8923e673cb3cdd
Quidden <dogemmoze@gmail.com>
2024-12-01 15:23:15 +0200

    add a get and sett in calass game player

--- Include/Person.h
+++ Include/Person.h
@@ -15,57 +15,85 @@ struct EquipResult
 
 class PersonC
 {
+protected:
+    int hp;
+    int damage;
+    int die_bonus;
+
 public:
     virtual ~PersonC() = default;
     bool IsAlive();
 
     void DisplayStatus();
-    PersonC(std::string name, std::string description, int hp, int damage, int die_bonus);
+    PersonC(int hp, int damage, int die_bonus);
 
-    std::string name;
-    std::string description;
-    int hp;
-    int damage;
-    int die_bonus;
+    virtual const std::string& GetClassName() const = 0;
+    virtual const std::string& GetClassDescription() const = 0;
 
+    int GetHP() const { return hp; }
+    int GetDamage() const { return damage; }
+    int GetDieBonus() const { return die_bonus; }
 };
 
 class PlayerC : public PersonC
 {
-public:
-
+protected:
     int wallet;
     int max_num_of_weapons;
 
     InventoryC inventory;
-    CharacterType charter_type;
+    std::vector<Equipted_Items_Base*> equipped_items;
 
     bool exit_the_battle = false;
     bool skip_hod = false;
 
-    PlayerC(std::string name, std::string description, int hp, int damage, int die_bonus, int wallet, int max_num_of_weapons, CharacterType charter_type);
-    void TryAbility(int chanceThreshold, bool &effectFlag, const std::string &abilityMessage);
+    CharacterType charter_type;
 
-    std::vector<Equipted_Items_Base *> equipped_items;
+public:
 
+    PlayerC(std::string name, std::string description, int hp, int damage, int die_bonus,
+            int wallet, int max_num_of_weapons, CharacterType charter_type);
+
+    int GetWallet() const { return wallet; }
+    void AddToWallet(int amount)
+    {
+        if (amount < 0 && wallet + amount < 0)
+            wallet = 0;
+        else
+            wallet += amount;
+    }
+
+    int GetMaxNumOfWeapons() const { return max_num_of_weapons; }
+
+    bool IsExitingBattle() const { return exit_the_battle; }
+    void SetExitBattle(bool value) { exit_the_battle = value; }
+
+    bool IsSkippingTurn() const { return skip_hod; }
+    void SetSkipTurn(bool value) { skip_hod = value; }
+
+    const InventoryC& GetInventory() const { return inventory; }
+    void AddToInventory(ItemC* item) { inventory.AddItem(item); }
+
+    const std::vector<Equipted_Items_Base*>& GetEquippedItems() const { return equipped_items; }
     EquipResult EquipError(int index);
     void EquipAction(Equipted_Items_Base* Equipted_Items, int index);
+
     bool UseItem(int index);
-    int AdjustDamage(bool add);
     bool SellItem(int index);
+
+    void TryAbility(int chanceThreshold, bool& effectFlag, const std::string& abilityMessage);
+    int AdjustDamage(bool add);
+
     virtual void Abilites() = 0;
-    virtual bool CanEquip(Equipted_Item_Type item_equip_seccess) const = 0;
+    virtual bool CanEquip(Equipted_Item_Type item_equip_success) const = 0;
 };
 
 class BastardC : public PlayerC
 {
 public:
     using PlayerC::PlayerC;
-    std::string description = "ss";
 
     std::vector<Equipted_Items_Base*> equpted_item;
-
-    bool exit_the_battle = false;
 
     void Abilites() override;
 
@@ -77,17 +105,26 @@ public:
 
 class KnightC : public PlayerC
 {
+private:
+    static const std::string className;
+    static const std::string classDescription;
+
 public:
     using PlayerC::PlayerC;
 
-    bool skip_hod = false;
-
-    void Abilites() override;
+    void Abilites() override
+    {
+        std::cout << GetClassName() << " blocks and skips the turn!" << std::endl;
+        SetSkipTurn(true);
+    }
 
     bool CanEquip(Equipted_Item_Type item_equip_seccess) const override
     {
         return item_equip_seccess == Equipted_Item_Type::Sword || item_equip_seccess == Equipted_Item_Type::Armor;
     }
+
+    const std::string& GetClassName() const override { return className; }
+    const std::string& GetClassDescription() const override { return classDescription; }
 };
 
 class BanditC : public PlayerC
