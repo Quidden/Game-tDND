f1b00be19751a1fd20c2f03770535be6042a9032
Quidden Laptop <dogemmoze@gmail.com>
2024-12-06 12:32:47 +0200

    Item class rework

--- Include/Item.h
+++ Include/Item.h
@@ -70,73 +70,49 @@ class SpecificWeapon : public Equipted_Items_Base
 class LongSword : public SpecificWeapon
 {
 public:
-	void CSWA2C() //Can stun with a 20% chance
-	{
 
-	}
 };
 
 class LSMagical : public SpecificWeapon
 {
 public:
-	void WA2CTHWMD() //With a 20% chance to hit with magic damage.
-	{
 
-	}
 };
 
 class LSLegendary : public SpecificWeapon
 {
 public:
-	void WA8COWAND() //With an 80% chance of winning any duel.
-	{
 
-	}
 };
 
 class dagger : public SpecificWeapon
 {
 public:
-	void WA1COKI() //With a 10% chance of killing instantly
-	{
 
-	}
 };
 
 class DMagical : public SpecificWeapon
 {
 public:
-	void WA5COKI() //With a 50% chance of killing instantly
-	{
 
-	}
 };
 
 class DLegendaryl : public SpecificWeapon
 {
 public:
-	void ILB1() //Increases luck by +10
-	{
 
-	}
 };
 
 class Bow : public SpecificWeapon
 {
 public:
-	void CTA1DC() //Can take a 10 damage crit
-	{
 
-	}
 };
 
 class BLegendary : public SpecificWeapon
 {
 public:
-	void AYTMTFAAHA9CTK() //Allows you to make the first attack and have a 90% chance to kill 
-	{
 
-	}
 };
 
 class Armor : public Equipted_Items_Base
